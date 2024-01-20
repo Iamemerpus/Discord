3b4f532a44bad2152c0c4171823237289888b59a
Emerpus <90596723+Iamemerpus@users.noreply.github.com>
2024-01-20 22:00:42 +0545

    Add files via upload

---
+++ README.md
@@ -0,0 +1,15 @@
+# Youtube Tutorial : https://www.youtube.com/watch?v=O2q7FefB1Zg
+
+# Streaming-24-7-RPC
+Allows you to keep your account status as streaming. It also allows you to set a gif link as your image in your rpc and have 2 buttons. It is fully customizable!
+
+# DO NOT GIVE YOUR TOKEN TO OTHERS!
+**Giving your token to someone else will give them the ability to log into your account without the password or 2FA.**
+
+# DO NOT INVITE SOMEBODY TO YOUR REPL!
+**Inviting somebody to your repl literally means that your giving them full access to your repl including your secrets tab.**
+
+> ⭐ Feel free to Star the Repository if this helped you! ;)
+>                                                                                
+> Streaming-24-7-RPC by mrnekrozyt is licensed under Attribution 4.0 International
+
---
+++ index.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js-selfbot-v13');
+const client = new Discord.Client({
+  readyStatus: false,
+  checkUpdate: false
+});
+
+const keepAlive = require('./server.js');
+keepAlive();
+
+function formatTime() { //Credits to himika#0001 and never#0001
+  const date = new Date();
+  const options = {
+    timeZone: 'America/New_York', //https://www.zeitverschiebung.net/en/ and find your city and enter here
+    hour12: true,
+    hour: 'numeric',
+    minute: 'numeric'
+  };
+  return new Intl.DateTimeFormat('en-US', options).format(date);
+}
+
+client.on('ready', async () => {
+  console.clear();
+  console.log(`${client.user.tag} - rich presence started!`);
+
+  const r = new Discord.RichPresence()
+    .setApplicationId('996312025623965756')
+    .setType('STREAMING')
+    .setURL('https://www.youtube.com/watch?v=dQw4w9WgXcQ') //Must be a youtube video link 
+    .setState('Recording')
+    .setName('Esther')
+    .setDetails(`Valorant [${formatTime()}]`)
+    .setStartTimestamp(Date.now())
+ .setAssetsLargeImage('https://media.discordapp.net/attachments/1099892530763538543/1197573641873924106/fbad87e7a1f248efbe29db90e2f47776.webp?') //You can put links in tenor or discord and etc.
+    .setAssetsLargeText('Boring') //Text when you hover the Large image
+    .setAssetsSmallImage('https://media.discordapp.net/attachments/1099892530763538543/1197573642347888680/287596f98304bf1adc2c411619ae8fef.jpg?') //You can put links in tenor or discord and etc.
+    .setAssetsSmallText('Twitch') //Text when you hover the Small image
+    .addButton('Watch', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ')
+    .addButton('Donate', 'https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+
+  client.user.setActivity(r);
+  client.user.setPresence({ status: "dnd" }); //dnd, online, idle, offline
+
+  let prevTime = null;
+  setInterval(() => {
+    const newTime = formatTime();
+    if (newTime !== prevTime) {
+      const newDetails = `[7/24] Uyanık 💀 [${newTime}]`;
+      r.setDetails(newDetails);
+      client.user.setActivity(r);
+      prevTime = newTime;
+    }
+  }, 1000); // Update every second
+});
+
+const mySecret = process.env['TOKEN'];
+client.login(mySecret);
