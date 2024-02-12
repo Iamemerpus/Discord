93f18f49d97d20beef459b1d6ca6cbf303e36c5a
vornex <79860992+vornex-gh@users.noreply.github.com>
2024-02-12 12:37:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -30,9 +30,9 @@ client.on('ready', async () => {
     .setName('randomness')
     .setDetails(`Hey 👋⚓ [${formatTime()}]`)
     .setStartTimestamp(Date.now())
- .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1150242860273254460/1206396977269440584/cd0qa1.gif') //You can put links in tenor or discord and etc.
+ .setAssetsLargeImage('https://cdn.discordapp.com/attachments/1150242860273254460/1206396977269440584/cd0qa1.gif?ex=65dbdbbd&is=65c966bd&hm=8b5855d5d1940919e2ca4c2aac25042a486c9035f52aa7f91efc8dc2d46a1cd5& //You can put links in tenor or discord and etc.
     .setAssetsLargeText('insert funny text') //Text when you hover the Large image
-    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1150242860273254460/1206396977269440584/cd0qa1.gif?ex=65dbdbbd&is=65c966bd&hm=8b5855d5d1940919e2ca4c2aac25042a486c9035f52aa7f91efc8dc2d46a1cd5&') //You can put links in tenor or discord and etc.
+    .setAssetsSmallImage('https://cdn.discordapp.com/attachments/1150242860273254460/1190250393746280509/cc5323ff-e634-4a9b-8320-2cb09efa665a.gif') //You can put links in tenor or discord and etc.
     .setAssetsSmallText('You weirdo, why would you hover over the small image.') //Text when you hover the Small image
     .addButton('My Site 🔨', 'https://vornexx.is-a.dev/')
     .addButton('In depth bio 💞', 'https://rentry.co/vornexx')
